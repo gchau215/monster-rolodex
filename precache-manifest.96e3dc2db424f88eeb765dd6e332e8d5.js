8d1b0915b8edc65412cf852ba5439087cf408f5a
gchau215 <garychau215@gmail.com>
2020-10-04 15:08:21 -0400

    Updates

---
+++ precache-manifest.96e3dc2db424f88eeb765dd6e332e8d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "897e31791e101722f2dc698a2853f6de",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "860001665048df8d9cf3",
+    "url": "/monster-rolodex/static/css/main.f8ec253d.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "860001665048df8d9cf3",
+    "url": "/monster-rolodex/static/js/main.c5887496.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
